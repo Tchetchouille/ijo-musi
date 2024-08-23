e5abf8fa1b89c0465b22f6fa418067c6af17a0c2
Tchetchouille <jeremias.kuehne@protonmail.com>
2024-08-23 15:47:13 +0200

    Started work on actual Godot project.

---
+++ Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/camera_controller.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@onready var CHARACTER: CharacterBody3D = $".."
+@export var SENSITIVITY: float = 0.003
+@export var TILT_LOWER_LIMIT = deg_to_rad(-90)
+@export var TILT_UPPER_LIMIT = deg_to_rad(90)
+
+
+func _ready():
+	# We change the mouse mode to capture inputs
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+# Apparently similar to _input() but gets called later?
+func _input(event):
+	# I the mouse is moved, we rotate do 2 things
+	if event is InputEventMouseMotion:
+		print("test")
+		# 1) We rotate the character's body and camera controller
+		CHARACTER.transform.basis = CHARACTER.transform.basis.rotated(CHARACTER.transform.basis.y, -event.relative.x * SENSITIVITY)
+		transform.basis = transform.basis.rotated(transform.basis.x, -event.relative.y * SENSITIVITY)
+		# 2) We tilt the camera controller in between constant limits
+		rotation.x = clamp(rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
---
+++ Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/game_manager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Handling our custom inputs (I think that's the way to do it?)
+func _input(event):
+	# Right now escape quits.
+	# Ultimately it should open the pause menu.
+	if event.is_action_pressed("escape"):
+		get_tree().quit()
