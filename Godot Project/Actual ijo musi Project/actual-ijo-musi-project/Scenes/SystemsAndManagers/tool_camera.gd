9fca09467efa64cf3c115319c47ffaa659f912e3
Tchetchouille <jeremias.kuehne@protonmail.com>
2024-09-24 16:37:21 +0200

    Added basic skull. Enabled custom shader for tool screen. Reduced candle shadow bias.

--- Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/SystemsAndManagers/tool_camera.gd
+++ Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/SystemsAndManagers/tool_camera.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	# Move and rotate the screen camera.
 	if tool.is_inside_tree():
 		position = tool.global_position
