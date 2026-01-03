d8435042b679c39565cc97d1044a3c682c910607
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2026-01-03 17:12:15 +0100

    implemented object detection when clicking on ilo toki's screen
    Clicking on ilo toki's screen now returns the clicked object.

--- Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/SystemsAndManagers/tool_screen.gd
+++ Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/SystemsAndManagers/tool_screen.gd
@@ -4,7 +4,7 @@ extends Area3D
 @onready var main_cam : Camera3D = $"../../../.."
 @onready var tool_cam : Camera3D = $"../../../../ToolViewport/ToolCamera"
 @onready var hand : Node3D = $"../../../Hand"
-@onready var local_ref : Node3D = $"../../../../ToolViewport/ToolCamera/LocalReference"
+@onready var tool_cam_front : Node3D = $"../../../../ToolViewport/ToolCamera/Tool_Camera_Front"
 
 var ray_length = 1000
 
@@ -20,30 +20,14 @@ func _input(event):
 		ray_to_screen.collide_with_bodies = false
 		var point_on_screen = get_world_3d().direct_space_state.intersect_ray(ray_to_screen)
 		if not point_on_screen.is_empty():
-			var old_vector = point_on_screen["position"] - tool_cam.global_position
-			
-			# project vector on the coordinates of the "screen space"
-			# since I have defined local_ref to have coordinates vectors of norm 1, the formula is simple:
-			var new_x = old_vector.dot(local_ref.get_child(0).global_position - tool_cam.global_position)
-			var new_y = old_vector.dot(local_ref.get_child(1).global_position - tool_cam.global_position)
-			var new_z = old_vector.dot(local_ref.get_child(2).global_position - tool_cam.global_position)
-			# Then I can apply my transformation to these new vectors
-			var fov_ratio = main_cam.fov / tool_cam.fov
-			new_x = new_x * fov_ratio
-			new_y = new_y * fov_ratio
-			# And finally use them to calculate the new vector
-			var new_vector = Vector3(new_x, new_y, new_z)		
-
-			# And now we can finally cast a ray from the tool to see if there is an object on its path
+			# ray properties
 			var ray_to_obj_start = tool_cam.global_position
-			var ray_to_obj_direction = (ray_to_obj_start + new_vector).normalized()
-			var ray_to_obj_end = ray_to_obj_direction * ray_length
+			var ray_to_obj_direction = point_on_screen["position"] + (tool_cam_front.global_position - ray_to_obj_start) * 0.45 - ray_to_obj_start
+			var ray_to_obj_end = ray_to_obj_start + ray_to_obj_direction * 100
+			# actually shooting the ray
 			var ray_to_obj = PhysicsRayQueryParameters3D.create(ray_to_obj_start, ray_to_obj_end)
-			ray_to_obj.collide_with_areas = false
-			ray_to_obj.collide_with_bodies = true
-			var hit = get_world_3d().direct_space_state.intersect_ray(ray_to_obj)
-			# debug
-			var test_scene = load("res://Scenes/Objects/Furniture/basic_lamp.tscn")
-			var instance = test_scene.instantiate()
-			$"../../../../../../..".add_child(instance)
-			instance.global_position = ray_to_obj_direction
+			ray_to_screen.collide_with_areas = false
+			ray_to_screen.collide_with_bodies = true
+			var obj = get_world_3d().direct_space_state.intersect_ray(ray_to_obj)
+			print(obj)
+		
