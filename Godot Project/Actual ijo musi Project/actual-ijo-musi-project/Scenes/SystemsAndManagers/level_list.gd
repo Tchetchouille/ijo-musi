bd402b463b53fdda5584f4192c021326b7bbb9a6
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2026-05-18 18:39:49 +0200

    Basic water and waterfall

--- Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/SystemsAndManagers/level_list.gd
+++ Godot Project/Actual ijo musi Project/actual-ijo-musi-project/Scenes/SystemsAndManagers/level_list.gd
@@ -1,12 +1,20 @@
 extends Node3D
 
 @onready var levels = [
-	preload("res://Scenes/Level Prototypes and Whiteboxing/waiting_room.tscn")
+	preload("res://Scenes/Level Prototypes and Whiteboxing/waiting_room.tscn"),
+	preload("res://Scenes/Level Prototypes and Whiteboxing/pond.tscn")
 ]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	load_level(1)
+
+func load_level(index):
+	var level = levels[index].instantiate()
+	add_child(level)
+	
+func load_all_levels():
 	for p_level in levels:
 		var level = p_level.instantiate()
 		add_child(level)
